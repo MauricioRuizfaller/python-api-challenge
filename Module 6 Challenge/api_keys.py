6ae4b60884400e8e33007aa45bf0792e1c84ab0d
Mauricio Ruiz Faller <124097568+MauricioRuizfaller@users.noreply.github.com>
2023-08-29 16:30:46 -0600

    Add files via upload

---
+++ Module 6 Challenge/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+
+# Geoapify API Key
+geoapify_key = "https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}"
